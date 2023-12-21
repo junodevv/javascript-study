7358b0da0fac0ccc1a820c54af87d6efcdf23082
leejuno <zhqkfxm0127@naver.com>
2023-12-21 18:37:46 +0900

    feat(var,let,const): 변수의 적용범위 실습

--- var,let,const/script.js
+++ var,let,const/script.js
@@ -1,3 +1,7 @@
+///////////////////////////
+/* 변수선언 재선언, 재할당 실습 */
+///////////////////////////
+
 // var: 같은 변수에 여러번 할당 가능
 var greeting = "hello";
 console.log(greeting);
@@ -20,4 +24,34 @@ console.log(greeting2);
 const greeting3 = 'hello';
 console.log(greeting3);
 
-greeting3 = 'hi';
+//greeting3 = 'hi';
+
+//////////////////////
+/* 변수의 참조 범위 실습 */
+//////////////////////
+
+// var = 함수레벨 스코프
+function func(){
+    if(true){
+        var a = 'a';
+        console.log(a);
+    }
+    console.log(a); // 함수 안에서는 참조가능
+}
+/** 함수 밖에서는 참조 불가능
+console.log(a); 
+ */
+func();
+
+// let, const => block 레벨 스코프
+function func2(){
+    if(true){
+        const a = 'a';
+        console.log(a); // 블록 내에서는 참조 가능
+    }
+    /** 블록 밖에서는 참조 불가능
+    console.log(a);
+     */
+}
+
+func2();
