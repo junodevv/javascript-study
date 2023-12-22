66fae557befcf6262dfcfcc86e1a30562c76861f
leejuno <zhqkfxm0127@naver.com>
2023-12-22 16:09:07 +0900

    feat(window): window object 실습

---
+++ window/script.js
@@ -0,0 +1,24 @@
+let val;
+
+val = window.outerHeight;
+console.log(val);
+val = window.outerWidth
+console.log(val);
+
+val = window.innerHeight;
+console.log(val);
+val = window.innerWidth;
+console.log(val);
+
+val = window.scrollY;
+console.log(val);
+val = window.scrollX;
+console.log(val);
+
+val = window.location
+console.log(val); // url에 대한 정보를 가지고 있다.
+
+//window.location.href = 'http://junodevv.github.io/'
+
+console.log(window.history);
+console.log(window.navigator);
